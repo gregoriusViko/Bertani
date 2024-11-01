8ff6d96a05267f8392a159cfa5a876f7c7ea7828
Nichemp <pratamanicholas515@gmail.com>
2024-11-01 15:01:12 +0700

    Membuat Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,20 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     function profile(){
-        return view('profile');
+        $user = Auth::guard('buyer')->check() ? Auth::guard('buyer')->user() : Auth::guard('farmer')->user();
+        $role = Auth::guard('buyer')->check() ? 'buyer ' : 'farmer';
+        return view('profile', compact('user', 'role'));
     }
 
-    function soba() {
-        return view('soba');
-    }
-
-    function aoka() {
-        return view('aoka');
-    }
 }
