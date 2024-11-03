038627329f309cf5cb7881ba38aaa1d47fbf9a91
gregoriusViko <vikoduiatmaka@gmail.com>
2024-11-03 12:49:31 +0700

    membuat middleware untuk admin

--- resources/views/components/navbar_admin.blade.php
+++ resources/views/components/navbar_admin.blade.php
@@ -1,5 +1,5 @@
 <x-navbar>
-    <x-nav-link href="#" :active="request()->is('')">Laporan</x-nav-link>
+    <x-nav-link href="{{ route('admin.laporan') }}" :active="request()->is('admin/laporan')">Laporan</x-nav-link>
     <x-nav-link href="#" :active="request()->is('')">Harga Pasar</x-nav-link>
     <x-nav-link href="#" :active="request()->is('')">Hapus Akun</x-nav-link>
 </x-navbar>
