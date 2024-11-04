3b2e0cd733d07130503dc1d86272a72abc3fa1e8
beetahdd <gregoriusangger93@gmai.com>
2024-11-04 21:18:06 +0700

    modif font and try for responsive sm-md

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,16 +5,16 @@
                 <img class="max-w-full max-h-full object-cover" src="/img/logo1.png" alt="Your Company">
             </div>
             <div
-                class="nav-links transition ease-in duration-200 md:static absolute bg-green-600 md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5 py-4">
-                <ul class="relative font-[hind] flex md:flex-row flex-col md:items-center md:gap-[2vw] gap-8 md:text-base text-lg">
+                class="nav-links transition ease-in duration-200 md:static absolute bg-green-600 md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5 py-1">
+                <ul class="relative font-hind flex md:flex-row flex-col md:items-center md:gap-[2vw] gap-8 md:text-base text-lg">
                     {{ $slot }}
                 </ul>
             </div>
             <div class="flex items-center gap-6">
                 <button
-                    class="bg-green-600 text-white px-4 py-1 relative font-[hind] text-1xl text-semibold rounded-full hover:bg-white hover:text-green-600 flex items-center justify-center space-x-2"
+                    class="bg-green-600 text-white px-4 py-1 relative font-hind text-1xl font-semibold rounded-full hover:bg-white hover:text-green-600 flex items-center justify-center space-x-2"
                     type="button">
-                    <span class="group-hover:text-green-600 relative font-[hind] text-1xl text-base text-semibold">
+                    <span class="group-hover:text-green-600 relative font-hind text-1xl text-base font-semibold">
                         @php
                             $user = null;
                             if (Auth::guard('admin')->check()) {
