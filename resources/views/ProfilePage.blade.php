e6cb24a587d0ac3491beac6de5a6282ad4450818
beetahdd <gregoriusangger93@gmail.com>
2024-11-04 22:57:33 +0700

    modif search bar and font

--- resources/views/ProfilePage.blade.php
+++ resources/views/ProfilePage.blade.php
@@ -9,34 +9,34 @@
                 <img class="rounded-full object-cover w-24 h-24" src="{{ $user->profile_img_link ? $user->profile_img_link : './img/orang.jpeg.jpg' }}" alt="avatar">
                 {{-- </div> --}}
             </div>
-            <h2 class="text-base font-bold text-black">{{$user->email}}</h2>
-            <h4 class="text-sm text-gray-700">{{$role}}</h4>
+            <h2 class="text-base font-libre-franklin font-bold text-black">{{$user->email}}</h2>
+            <h4 class="font-libre-franklin font-normal text-sm text-gray-700">{{$role}}</h4>
         </div>
         {{-- form --}}
         <form class="mt-6 max-w-sm mx-auto " id="profile-form" action="{{route('profile.update')}}" method="POST">
             @csrf
             <div class="mb-5">
-                <label for="nama-input" class="block mb-1 text-base font-semibold  text-black">Nama Pengguna</label>
+                <label for="nama-input" class="block mb-1 text-base font-libre-franklin font-semibold  text-black">Nama Pengguna</label>
                 <input type="text" id="nama-input" name="name"
-                    class="bg-gray-50 border mb-2 border-gray-300 text-black text-base items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    class="bg-gray-50 border mb-2 border-gray-300 text-black text-base font-libre-franklin font-normal items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                     value="{{$user->name}}" required readonly />
             </div>
             <div class="mb-5">
-                <label for="alamat-input" class="block mb-1 text-base font-semibold  text-black">Alamat</label>
+                <label for="alamat-input" class="block mb-1 text-base font-libre-franklin font-semibold  text-black">Alamat</label>
                 <input type="text" id="alamat-input" name="home_address"
-                    class="bg-gray-50 border mb-2 border-gray-300 text-black text-base items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    class="bg-gray-50 border mb-2 border-gray-300 text-black text-base font-libre-franklin font-normal items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                     value="{{$user->home_address}}" required readonly />
             </div>
             <div class="mb-5">
-                <label for="notelp-input" class="block mb-1 text-base font-semibold  text-black">No Telepon</label>
+                <label for="notelp-input" class="block mb-1 text-base font-libre-franklin font-semibold  text-black">No Telepon</label>
                 <input type="tel" id="notelp-input" name="phone_number"
-                    class="bg-gray-50 border mb-2 border-gray-300 text-black text-base items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    class="bg-gray-50 border mb-2 border-gray-300 text-black text-base font-libre-franklin font-normal items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     value="{{$user->phone_number}}" required readonly />
             </div>
             <div class="mb-5">
-                <label for="email-input" class="block mb-1 text-base font-semibold  text-black">Email</label>
+                <label for="email-input" class="block mb-1 text-base font-libre-franklin font-semibold  text-black">Email</label>
                 <input type="text" id="email-input" name="email_address"
-                    class="bg-gray-50 border mb-2 border-gray-300 text-black text-base items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    class="bg-gray-50 border mb-2 border-gray-300 text-black text-base font-libre-franklin font-normal items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                     value="{{$user->email_address}}" required readonly />
             </div>
         </form>
