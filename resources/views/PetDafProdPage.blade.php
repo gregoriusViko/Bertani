a28b369876d229a34b755e9de9cd86e278587342
beetahdd <gregoriusangger93@gmail.com>
2024-11-04 21:18:06 +0700

    modif font and try for responsive sm-md

--- resources/views/PetDafProdPage.blade.php
+++ resources/views/PetDafProdPage.blade.php
@@ -1,14 +1,14 @@
 <x-layout>
     <x-slot:title>Daftar Produk-Bertani.com</x-slot:title>
     <div dir="ltr">
-        <div class="mx-auto max-w-7xl px-4 mt-5 mb-4 sm:px-6 lg:px-8 flex justify-between items-center">
+        <div class="font-libre-franklin font-bold mx-auto max-w-7xl px-4 mt-5 mb-4 sm:px-6 lg:px-8 flex justify-between items-center">
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">Daftar Produk</h1>
 
             <div dir="rtl">
                 <button
-                    class="items-center justify-center text-white bg-green-300 px-4 py-1 rounded-lg hover:bg-green-600"
+                    class="font-libre-franklin font-light flex items-center justify-center text-white bg-green-300 px-4 py-1 rounded-lg hover:bg-green-600"
                     type="button" id="addProduct-button" onclick="toggleAllInputs()">
-                    <span><ion-icon name="add-circle-outline" class="ml-2 text"></ion-icon></span>
+                    <span><ion-icon name="add-circle-outline" class="ml-2 mt-1.5 text-lg"></ion-icon></span>
                     Tambah Produk
                 </button>
             </div>
@@ -25,24 +25,24 @@
                 <img src="./img/logo3.jpg" alt="profile" class="thumbnail md:w-full md:h-full lg:w-full lg:h-full sm:w-1/2 sm:h-1/2 object-cover">
             </div>
             <!-- Nama Produk -->
-            <div class="sm:col-span-1 md:col-span-3 lg:col-span-4 sm:text-sm md:text-base lg:text-lg lg:ml-5 md:-ml-5 font-semibold ">
+            <div class="font-libre-franklin font-semibold sm:col-span-1 md:col-span-3 lg:col-span-4 sm:text-sm md:text-base lg:text-lg lg:ml-5 md:-ml-5">
                 {{-- {{ $product->name }} --}} "Nama Produk"
             </div>
             <!-- Jenis Produk -->
-            <div class="sm:col-span-1 md:row-start-3 md:col-start-4 md:col-span-3 lg:row-start-3 lg:col-start-3 md:-ml-5  lg:ml-5 lg:col-span-4 sm:text-sm md:text-base lg:text-lg text-gray-600">
+            <div class="font-libre-franklin font-medium sm:col-span-1 md:row-start-3 md:col-start-4 md:col-span-3 lg:row-start-3 lg:col-start-3 md:-ml-5  lg:ml-5 lg:col-span-4 sm:text-sm md:text-base lg:text-lg text-gray-600">
                 {{-- {{ $product->product_type }} --}} Jenis Produk:
             </div>
             <!-- Harga Produk -->
-            <div class="sm:col-span-1 md:col-start-7 md:col-span-2 lg:row-start-1 lg:col-start-7 lg:col-span-2 sm:text-sm md:text-base lg:text-lg md:flex md:justify-end  text-gray-600">
+            <div class="font-libre-franklin font-medium sm:col-span-1 md:col-start-7 md:col-span-2 lg:row-start-1 lg:col-start-7 lg:col-span-2 sm:text-base md:text-lg lg:text-2xl md:flex md:justify-end  text-gray-600">
                 {{-- {{ $product->price }} --}} Rp xx.xxx
             </div>
             <!-- Jumlah Stok -->
-            <div class="sm:col-span-1 md:row-start-5 md:col-start-4 md:col-span-3 lg:row-start-5 lg:col-start-3 md:-ml-5 lg:ml-5 lg:col-span-4 sm:text-sm md:text-base lg:text-lg text-gray-600">
+            <div class="font-libre-franklin font-medium sm:col-span-1 md:row-start-5 md:col-start-4 md:col-span-3 lg:row-start-5 lg:col-start-3 md:-ml-5 lg:ml-5 lg:col-span-4 sm:text-sm md:text-base lg:text-lg text-gray-600">
                 {{-- {{ $product->stock }} --}} Jumlah Stok:
             </div>
             <!-- Tombol Aksi -->
             <div class="sm:col-span-2 md:row-start-5 md:col-start-7 md:col-span-2 lg:col-start-7 lg:col-span-2 lg:row-start-5 md:flex md:justify-end space-x-2 sm:text-sm md:text-base lg:text-lg  ">
-                <button class="rounded-md hover:text-blue-500"><ion-icon name="create-outline" class="transition ease-in duration-100 text-xl"></ion-icon></button>
+                <button class="rounded-md hover:text-blue-500"><ion-icon name="create-outline" class="transition ease-in duration-100 text-2xl"></ion-icon></button>
                 <button onclick="toggleComponent()" class="rounded-md  hover:text-red-500"><ion-icon name="trash-outline"
                         class="transition ease-in duration-100 text-xl"></ion-icon></button>
             </div>
