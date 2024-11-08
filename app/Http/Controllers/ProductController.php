3e758d685f021e112fb7646b9627304081d69385
Nichemp <pratamanicholas515@gmail.com>
2024-11-08 13:55:26 +0700

    Fungsi Add Produk

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,10 +24,36 @@ class ProductController extends Controller
         $products = Product::with('farmer')->paginate(perPage: 8);
         return view('partials.product', compact('products'))->render();
     }
-
     public function farmerProducts(){
         $farmer = Auth::guard('farmer')->user();
         $products = $farmer->products;
         return view('PetDafProdPage', compact('products'));
     }
+
+    public function create() {
+        return view('product.create');
+    }
+
+    public function Toko(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'price' => 'required|numeric',
+            'description' => 'required',
+            'stock_kg' => 'required|decimal',
+            'selling_unit_kg' => 'required|decimal',
+        ]);
+
+        Product::create([
+            'name' => $request->name,
+            'price' => $request->price,
+            'description' => $request->description,
+            'stock_kg' => $request->stock_kg,
+            'selling_unit_kg' => $request->selling_unit_kg,
+            'product_type' => $request->product_type,
+            'img_link' => $request->img_link,
+        ]);
+
+        return redirect()->route('')->with('success', 'sukses menambahkan produk');
+    }
+
 }
