9fc812b69f6dc7e986e3bd6a2181f98fe86879d6
beetahdd <gregoriusangger93@gmai.com>
2024-11-08 14:13:38 +0700

    add view and component for tambah produk

--- resources/views/PetDafProdPage.blade.php
+++ resources/views/PetDafProdPage.blade.php
@@ -18,36 +18,36 @@
     <!-- bawah ini adalah component untuk produk -->
     <div id="cardContainer"
         class="mx-auto max-w-7xl px-4 py-1 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-6">
-        {{-- @foreach ($products as $product) --}}
-        <div class="shadow-lg border rounded-lg p-4 grid sm:grid-cols-2 sm:grid-flow-row md:grid-cols-8 md:grid-flow-row lg:grid-cols-8 lg:grid-flow-row gap-4 items-start">
-            <!-- Gambar Produk -->
-            <div class="sm:col-span-1 sm:row-span-2 md:col-span-3 md:row-span-5 lg:col-span-2 lg:row-span-5 flex justify-center items-center border rounded-lg sm:w-24 sm:h-24 lg:w-60 lg:h-48 md:w-56 md:h-44 md:mt-1 overflow-hidden">
-                <img src="./img/logo3.jpg" alt="profile" class="thumbnail md:w-full md:h-full lg:w-full lg:h-full sm:w-1/2 sm:h-1/2 object-cover">
+        @foreach ($products as $product)
+            <div class="shadow-lg border rounded-lg p-4 grid sm:grid-cols-2 sm:grid-flow-row md:grid-cols-8 md:grid-flow-row lg:grid-cols-8 lg:grid-flow-row gap-4 items-start">
+                <!-- Gambar Produk -->
+                <div class="sm:col-span-1 sm:row-span-2 md:col-span-3 md:row-span-5 lg:col-span-2 lg:row-span-5 flex justify-center items-center border rounded-lg sm:w-24 sm:h-24 lg:w-60 lg:h-48 md:w-56 md:h-44 md:mt-1 overflow-hidden">
+                    <img src="./img/logo3.jpg" alt="profile" class="thumbnail md:w-full md:h-full lg:w-full lg:h-full sm:w-1/2 sm:h-1/2 object-cover">
+                </div>
+                <!-- Nama Produk -->
+                <div class="font-libre-franklin font-semibold sm:col-span-1 md:col-span-3 lg:col-span-4 sm:text-sm md:text-base lg:text-lg lg:ml-5 md:-ml-5">
+                    {{ $product->name }}
+                </div>
+                <!-- Jenis Produk -->
+                <div class="font-libre-franklin font-medium sm:col-span-1 md:row-start-3 md:col-start-4 md:col-span-3 lg:row-start-3 lg:col-start-3 md:-ml-5  lg:ml-5 lg:col-span-4 sm:text-sm md:text-base lg:text-lg text-gray-600">
+                    Jenis Produk: {{ $product->product_type }}
+                </div>
+                <!-- Harga Produk -->
+                <div class="font-libre-franklin font-medium sm:col-span-1 md:col-start-7 md:col-span-2 lg:row-start-1 lg:col-start-7 lg:col-span-2 sm:text-base md:text-lg lg:text-2xl md:flex md:justify-end  text-gray-600">
+                    {{ Number::currency($product->price, in: 'idr') }}
+                </div>
+                <!-- Jumlah Stok -->
+                <div class="font-libre-franklin font-medium sm:col-span-1 md:row-start-5 md:col-start-4 md:col-span-3 lg:row-start-5 lg:col-start-3 md:-ml-5 lg:ml-5 lg:col-span-4 sm:text-sm md:text-base lg:text-lg text-gray-600">
+                    Jumlah Stok: {{ WeightConverter::convert($product->stock_kg) }}
+                </div>
+                <!-- Tombol Aksi -->
+                <div class="sm:col-span-2 md:row-start-5 md:col-start-7 md:col-span-2 lg:col-start-7 lg:col-span-2 lg:row-start-5 md:flex md:justify-end space-x-2 sm:text-sm md:text-base lg:text-lg  ">
+                    <button class="rounded-md hover:text-blue-500"><ion-icon name="create-outline" class="transition ease-in duration-100 text-2xl"></ion-icon></button>
+                    <button onclick="toggleComponent()" class="rounded-md  hover:text-red-500"><ion-icon name="trash-outline"
+                            class="transition ease-in duration-100 text-xl"></ion-icon></button>
+                </div>
             </div>
-            <!-- Nama Produk -->
-            <div class="font-libre-franklin font-semibold sm:col-span-1 md:col-span-3 lg:col-span-4 sm:text-sm md:text-base lg:text-lg lg:ml-5 md:-ml-5">
-                {{-- {{ $product->name }} --}} "Nama Produk"
-            </div>
-            <!-- Jenis Produk -->
-            <div class="font-libre-franklin font-medium sm:col-span-1 md:row-start-3 md:col-start-4 md:col-span-3 lg:row-start-3 lg:col-start-3 md:-ml-5  lg:ml-5 lg:col-span-4 sm:text-sm md:text-base lg:text-lg text-gray-600">
-                {{-- {{ $product->product_type }} --}} Jenis Produk:
-            </div>
-            <!-- Harga Produk -->
-            <div class="font-libre-franklin font-medium sm:col-span-1 md:col-start-7 md:col-span-2 lg:row-start-1 lg:col-start-7 lg:col-span-2 sm:text-base md:text-lg lg:text-2xl md:flex md:justify-end  text-gray-600">
-                {{-- {{ $product->price }} --}} Rp xx.xxx
-            </div>
-            <!-- Jumlah Stok -->
-            <div class="font-libre-franklin font-medium sm:col-span-1 md:row-start-5 md:col-start-4 md:col-span-3 lg:row-start-5 lg:col-start-3 md:-ml-5 lg:ml-5 lg:col-span-4 sm:text-sm md:text-base lg:text-lg text-gray-600">
-                {{-- {{ $product->stock }} --}} Jumlah Stok:
-            </div>
-            <!-- Tombol Aksi -->
-            <div class="sm:col-span-2 md:row-start-5 md:col-start-7 md:col-span-2 lg:col-start-7 lg:col-span-2 lg:row-start-5 md:flex md:justify-end space-x-2 sm:text-sm md:text-base lg:text-lg  ">
-                <button class="rounded-md hover:text-blue-500"><ion-icon name="create-outline" class="transition ease-in duration-100 text-2xl"></ion-icon></button>
-                <button onclick="toggleComponent()" class="rounded-md  hover:text-red-500"><ion-icon name="trash-outline"
-                        class="transition ease-in duration-100 text-xl"></ion-icon></button>
-            </div>
-        </div>
-        {{-- @endforeach --}}
+        @endforeach
     </div>
 
     <div id="componentContainer" class="hidden mt-4">
