fe19ba6dd53a4c0603e98cca40c60b1d1fcc09be
gregoriusViko <vikoduiatmaka@gmail.com>
2024-11-12 00:39:26 +0700

    mencoba menambahkan verifikasi

--- app/Models/Farmer.php
+++ app/Models/Farmer.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Farmer extends Authenticatable
+class Farmer extends Authenticatable implements MustVerifyEmail
 {
     protected $fillable = [
         'name',
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,39 +8,38 @@
             </div>
             <div
                 class="nav-links transition ease-in duration-200 md:static absolute bg-green-600 md:min-h-fit left-0 top-[-100%] md:w-auto  w-full flex items-center px-5 py-4">
-                <ul class="relative font-hind flex flex-col md:flex-row md:items-center md:gap-[2vw] md:text-base text-lg">
+                <ul
+                    class="relative font-hind flex flex-col md:flex-row md:items-center md:gap-[2vw] md:text-base text-lg">
                     {{ $slot }}
                 </ul>
             </div>
             <div class="flex items-center gap-6">
-                <button
-                    class="bg-green-600 text-white px-4 py-1 relative font-hind text-1xl font-semibold rounded-full hover:bg-white hover:text-green-600 flex items-center justify-center space-x-2"
-                    type="button">
-                    <span class="group-hover:text-green-600 relative font-hind text-1xl text-base font-semibold">
-                        @php
-                            $user = null;
-                            if (Auth::guard('admin')->check()) {
-                                $user = Auth::guard('admin')->user();
-                            } elseif (Auth::guard('buyer')->check()) {
-                                $user = Auth::guard('buyer')->user();
-                            } elseif (Auth::guard('farmer')->check()) {
-                                $user = Auth::guard('farmer')->user();
-                            }
-                        @endphp
-
-                        @if ($user)
-                            <a href="{{ route('profile') }}">{{ $user->email_address }}</a>
+                @php
+                    $user = null;
+                    if (Auth::guard('admin')->check()) {
+                        $user = Auth::guard('admin')->user();
+                    } elseif (Auth::guard('buyer')->check()) {
+                        $user = Auth::guard('buyer')->user();
+                    } elseif (Auth::guard('farmer')->check()) {
+                        $user = Auth::guard('farmer')->user();
+                    }
+                @endphp
+                <a href="{{ $user ? route('profile') : '/login' }}">
+                    <button
+                        class="bg-green-600 text-white px-4 py-1 relative font-hind text-1xl font-semibold rounded-full hover:bg-white hover:text-green-600 flex items-center justify-center space-x-2"
+                        type="button">
+                        <span class="group-hover:text-green-600 relative font-hind text-1xl text-base font-semibold">
+                            {{ $user ? $user->email_address : 'Belum Login' }}
+                        </span>
+                        @if ($user && $user->profile_img_link)
+                            <img src="{{ $user->profile_img_link }}" alt=""
+                                class="w-10 h-10 rounded-full object-cover group-hover:text-green-600">
                         @else
-                            <a href="/login">Belum Login</a>
+                            <ion-icon name="person-circle-outline"
+                                class="text-2xl group-hover:text-green-600"></ion-icon>
                         @endif
-                    </span>
-                    @if ($user && $user->profile_img_link)
-                        <img src="{{ $user->profile_img_link }}" alt=""
-                            class="w-10 h-10 rounded-full object-cover group-hover:text-green-600">
-                    @else
-                        <ion-icon name="person-circle-outline" class="text-2xl group-hover:text-green-600"></ion-icon>
-                    @endif
-                </button>
+                    </button>
+                </a>
                 <ion-icon onclick="onToggleMenu(this)" name="menu"
                     class="text-1xl cursor-pointer md:hidden text-white"></ion-icon>
             </div>
