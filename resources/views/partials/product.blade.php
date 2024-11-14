dae8d8f0151c894e4a214391f3f1518a9907eda4
gregoriusViko <vikoduiatmaka@gmail.com>
2024-11-14 17:08:21 +0700

    memperbaiki bagian home

--- resources/views/partials/product.blade.php
+++ resources/views/partials/product.blade.php
@@ -4,7 +4,7 @@
             alt="">
         <div class="p-2 grid-cols-2">
             <div class="col-span-2 text-base font-mono">
-                {{ $product->type->name }} - {{ WeightConverter::convert($product->selling_unit_kg) }}
+                {{ $product->type->name }}
             </div>
             <div class="text-xl font-mono font-bold">
                 {{ Number::currency($product->price, in: 'idr') }}
