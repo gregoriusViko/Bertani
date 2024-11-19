bf8e63fe136e8252d86823d824c9d14897ce2425
gregoriusViko <vikoduiatmaka@gmail.com>
2024-11-19 10:51:25 +0700

    memperbaiki pesanan

--- resources/views/components/navbar_pembeli.blade.php
+++ resources/views/components/navbar_pembeli.blade.php
@@ -2,7 +2,7 @@
     <x-nav-link href="/" :active="request()->is('/')">Beranda</x-nav-link>
     <x-nav-link href="/hargapasar" :active="request()->is('hargapasar')">Harga Pasar</x-nav-link>
     <x-nav-link href="/produk" :active="request()->is('produk')">Produk</x-nav-link>
-    <x-nav-link href="/pesanan" :active="request()->is('pesanan')">Pesanan</x-nav-link>
+    <x-nav-link href="/pembeli/pesanan" :active="request()->is('pesanan')">Pesanan</x-nav-link>
     <x-nav-dropdown>
         <x-slot:title>Lainnya</x-slot:title>
         <x-dropdown-list href="/chat" :active="request()->is('chat')">Chat</x-dropdown-list>
