a073018344043aba582764b216b7ec11d7325175
Nichemp <pratamanicholas515@gmail.com>
2024-11-30 01:00:49 +0700

    Pembuatan Pesanan dari controller, dll

---
+++ app/Models/HistoryPrice.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class HistoryPrice extends Model
+{
+    /** @use HasFactory<\Database\Factories\HistoryPriceFactory> */
+    use HasFactory;
+    protected $guarded = ['id','created_at','updated_at'];
+    protected $with = ['product'];
+
+    public function order(): HasMany{
+        return $this->hasMany(Order::class);
+    }
+    
+    public function product(): BelongsTo{
+        return $this->belongsTo(Product::class);
+    }
+}
