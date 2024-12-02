b2f10622baac6338d37c88db3ed705e22bb73fb3
gregoriusViko <vikoduiatmaka@gmail.com>
2024-12-02 18:45:55 +0700

    mencoba menambahkan real time

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -3,37 +3,54 @@
 namespace App\Livewire;
 
 use App\Models\Buyer;
+use App\Events\Typing;
 use App\Models\Farmer;
 use Livewire\Component;
 use App\Models\BuyerChat;
 use App\Models\FarmerChat;
+use App\Events\MessageSent;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Echo;
+use Illuminate\Support\Facades\Broadcast;
 
 class Chat extends Component
 {
     public $user, $role, $chats, $content, $contacts, $interlocutor, $message;
 
-    public function mount($slug = null){
+    public function getListeners(){
+        return [
+            "echo-private:chat.{$this->role}.{$this->user->id},client-typing" => 'getTyping'
+        ];
+    }
+
+    public function receiveMessage($payload)
+    {
+        $this->chats->push($payload['message']);
+        $this->content = $this->chats->where('buyer_id', $this->interlocutor->id);
+    }
+
+    public function mount($slug = null)
+    {
         $this->role = Auth::guard('farmer')->check() ? 'farmer' : 'buyer';
         $this->user = Auth::guard($this->role)->user();
-        if($this->role == 'farmer'){
+        if ($this->role == 'farmer') {
             $this->chats = $this->user->farmerChats;
             $this->contacts = $this->chats->unique('buyer_id')->pluck('buyer');
             $buyer = $slug ? Buyer::where('slug', $slug)->first() : null;
-            if($buyer){
+            if ($buyer) {
                 $this->interlocutor = $buyer;
                 $this->content = $this->chats->where('buyer_id', $buyer->id);
-            }elseif($slug){
+            } elseif ($slug) {
                 abort(404);
             }
-        }else{
+        } else {
             $this->chats = $this->user->buyerChats;
             $this->contacts = $this->chats->unique('farmer_id')->pluck('farmer');
-            $farmer = $slug ? Buyer::where('slug', $slug)->first() : null;
-            if($farmer){
+            $farmer = $slug ? Farmer::where('slug', $slug)->first() : null;
+            if ($farmer) {
                 $this->interlocutor = $farmer;
                 $this->content = $this->chats->where('buyer_id', $farmer->id);
-            }elseif($slug){
+            } elseif ($slug) {
                 abort(404);
             }
         }
@@ -43,11 +60,16 @@ class Chat extends Component
         return view('livewire.chat')->layout('components.layout');
     }
 
-    public function kirimPesan(){
-        if($this->role == 'farmer'){
+    public function getTyping(){
+        dd('dsfdsf');
+    }
+
+    public function kirimPesan()
+    {
+        if ($this->role == 'farmer') {
             $farmer_id = $this->user->id;
             $buyer_id = $this->interlocutor->id;
-        }else{
+        } else {
             $buyer_id = $this->user->id;
             $farmer_id = $this->interlocutor->id;
         }
@@ -57,7 +79,9 @@ class Chat extends Component
             'farmer_id' => $farmer_id,
             'buyer_id' => $buyer_id
         ];
-        FarmerChat::create($message + ['role' => $this->role == 'farmer' ? 'sender' : 'receiver']);
+        $newMessage = FarmerChat::create($message + ['role' => $this->role == 'farmer' ? 'sender' : 'receiver']);
         BuyerChat::create($message + ['role' => $this->role == 'buyer' ? 'sender' : 'receiver']);
+
+        broadcast(new MessageSent($newMessage))->toOthers();
     }
 }
