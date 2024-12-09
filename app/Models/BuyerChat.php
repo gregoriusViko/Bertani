8445d66602871a03546cbdcc20d27fe4c2373114
gregoriusViko <vikoduiatmaka@gmail.com>
2024-12-09 23:44:44 +0700

    menambahkan chat di backend (sudah cukup baik)

--- app/Models/BuyerChat.php
+++ app/Models/BuyerChat.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class BuyerChat extends Model
 {
     use HasFactory;
-    public $updated_at = false;
+    const UPDATED_AT = null;
     protected $guarded = ['send_time'];
     const CREATED_AT = 'send_time';
     protected $with = ['buyer', 'farmer'];
