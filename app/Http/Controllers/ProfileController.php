3d2e0ca47686b335dd1c4f0287f88826c38debc9
Nichemp <pratamanicholas515@gmail.com>
2024-12-10 15:18:56 +0700

    nyoba benerin profile, tp masih blm bisa

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
