a41e20cb48c3634261fc16a899a14afbfb6f3c2b
gregoriusViko <vikoduiatmaka@gmail.com>
2024-12-15 18:13:31 +0700

    menambah tanda untuk chat

--- resources/views/components/navbar_petani.blade.php
+++ resources/views/components/navbar_petani.blade.php
@@ -10,17 +10,41 @@
     </x-nav-dropdown>
     <x-nav-dropdown>
         <x-slot:icon>
-            <span class="absolute top-1 right-0 transform translate-x-1/2 -translate-y-1/2 flex h-2 w-2">
+            <span id="notif-chat" class="absolute top-1 right-0 transform translate-x-1/2 -translate-y-1/2 flex h-2 w-2"
+                style="visibility: hidden;">
                 <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
                 <span class="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
             </span>
-            
-            <script>
-                
-            </script>
         </x-slot:icon>
         <x-slot:title>Lainnya</x-slot:title>
-        <x-dropdown-list href="/chat" :active="request()->is('chat')">Chat</x-dropdown-list>
+        <x-dropdown-list href="/chat" :active="request()->is('chat')">
+            Chat
+            <div id="jumlah-chat"
+                class="inline-block ml-2 bg-green-500 text-white text-xs font-bold px-1 py-1 rounded-full min-w-[20px] text-center justify-center"
+                style="visibility: hidden;">0</div>
+        </x-dropdown-list>
         <x-dropdown-list href="/laporan/sistem" :active="request()->is('laporan-sistem')">Laporan</x-dropdown-list>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                let notifChat = document.getElementById('notif-chat');
+                let jumlahChat = document.getElementById('jumlah-chat');
+                axios.get("{{ route('sum-of-chat') }}").then((response) => {
+                    console.log(response.data);
+                    if (response.data > 0) {
+                        notifChat.style.visibility = "visible";
+                        jumlahChat.style.visibility = "visible";
+                        jumlahChat.innerHTML = response.data;
+                    }
+                });
+
+                Echo.private(`chat.farmer.{{ Auth::guard('farmer')->user()->slug }}`)
+                    .listen("MessageSent", (response) => {
+                        notifChat.style.visibility = "visible";
+                        jumlahChat.style.visibility = "visible";
+                        jumlahChat.innerHTML = parseInt(jumlahChat.innerHTML) + 1;
+                    })
+            });
+        </script>
     </x-nav-dropdown>
 </x-navbar>
