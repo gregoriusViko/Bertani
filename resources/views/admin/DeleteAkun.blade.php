8288a9d264c7ba23703a408eebf9f9aa386730e5
beetahdd <gregoriusangger93@gmail.com>
2024-12-16 00:49:46 +0700

    menambah modal message berhasil saat melapor, mengirim tanggapan dan menghapus laporan

--- resources/views/admin/DeleteAkun.blade.php
+++ resources/views/admin/DeleteAkun.blade.php
@@ -6,13 +6,13 @@
             <label for="cariAkun" class="text-xs md:text-base lg:text-lg text-black px-4 font-semibold">Cari Akun</label>
             <input type="text" id="cariAkun" name="email"
                 class="pl-3 border border-black rounded-md w-1/2 block font-libre-franklin font-medium text-xs md:text-base text-black"
-                required>
+                placeholder="Masukkan nama email" required>
             <button id="buttonCari" type="submit"
                 class="text-xs md:text-base lg:text-lg rounded-md bg-blue-500 text-white hover:bg-blue-700 px-3 ">OK</button>
         </form>
 
         @isset($user)
-            <div class="rounded-md mt-8 px-7 mx-auto max-w-screen-lg bg-blue-400 py-4">
+            <div class="rounded-md mt-8 px-7 mx-auto max-w-screen-lg bg-[#A0D683] py-4" id="kotakHasil">
                 <div class=" grid grid-flow-row gap-4  items-center place-items-center ">
                     <div class="items-center place-items-center">
                         <div class="avatar mb-3 w-20 h-20  ">
@@ -46,7 +46,37 @@
                             class="bg-gray-50 border mb-2 border-gray-300 text-black text-xs md:text-base font-libre-franklin font-normal items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                             value="{{ $user->phone_number }}" required readonly />
                     </div>
-                    <div id="button" class="my-3 flex place-items-end ">
+                    @if ($role == 'farmer')
+                        <div class="w-full md:w-1/2">
+                            <label for="bank-input"
+                                class="block mb-1 text-xs md:text-base font-libre-franklin font-semibold  text-black">Bank</label>
+                            <input type="tel" id="notelp-input" name="phone_number"
+                                class="bg-gray-50 border mb-2 border-gray-300 text-black text-xs md:text-base font-libre-franklin font-normal items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                value="{{ $user->bank }}" required readonly />
+                        </div>
+                        <div class="w-full md:w-1/2">
+                            <label for="norek-input"
+                                class="block mb-1 text-xs md:text-base font-libre-franklin font-semibold  text-black">Nomor
+                                Rekening</label>
+                            <input type="tel" id="notelp-input" name="phone_number"
+                                class="bg-gray-50 border mb-2 border-gray-300 text-black text-xs md:text-base font-libre-franklin font-normal items-center pl-3 py-1 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                value="{{ $user->nomor_rekening }}" required readonly />
+                        </div>
+                    @endif
+                    <div class="w-full md:w-1/2">
+                        <p class="block mb-1 text-xs md:text-base font-libre-franklin font-semibold  text-black">Tuliskan
+                            Keterangan Alasan Penghapusan Akun</p>
+                        <textarea id="alasanHapusAkun" name="alasanHapusAkun" class="w-full rounded-lg p-2 mb-2 resize-none" rows="4"
+                            placeholder="Masukkan alasan penghapusan akun"required></textarea>
+                        <div class="flex justify-end mb-3">
+                            <button onclick="enableDeleteButton()" id="buttonKirimEmail"
+                                class="rounded-md text-xs md:text-base bg-black hover:bg-gray-600 text-white px-2 py-1">KIRIM
+                                KE EMAIL
+                            </button>
+                        </a>
+                    </div>
+
+                    <div id="2button" class="my-3 flex justify-center">
                         <div class="flex items-end space-x-2 justify-between">
                             <a href="{{ route('DeleteAkun') }}">
                                 <button onclick="showPopup('teruskan')" id="buttonBatal"
@@ -57,8 +87,8 @@
                                 @csrf
                                 @method('DELETE')
                                 <button onclick="showPopup('teruskan')" type="submit" name="id" id="buttonDelete"
-                                    value="{{ $user->id }}"
-                                    class="rounded-md text-xs md:text-base bg-gray-400 text-white hover:bg-red-600 px-2 py-1">HAPUS
+                                    value="{{ $user->id }}" disabled
+                                    class="rounded-md text-xs md:text-base bg-gray-400 text-white px-2 py-1">HAPUS
                                     AKUN</button>
                             </form>
                         </div>
@@ -98,6 +128,16 @@
         @enderror
     </div>
     <Script>
+        function enableDeleteButton() {
+            // Aktifkan tombol "HAPUS AKUN"
+            const deleteButton = document.getElementById('buttonDelete');
+            deleteButton.disabled = false;
+
+            // Ubah warna tombol setelah aktif
+            deleteButton.classList.remove('bg-gray-400');
+            deleteButton.classList.add('bg-red-400', 'hover:bg-red-600');
+        }
+
         function closeMessage(elementId) {
             const messageElement = document.getElementById(elementId);
             if (messageElement) {
@@ -113,13 +153,13 @@
                 setTimeout(() => {
                     notification.style.display = 'none';
                 }, 5000); // Notifikasi akan hilang setelah 5 detik
-            }  // Hilangkan pesan error setelah 3 detik
+            } // Hilangkan pesan error setelah 3 detik
             const errorMessage = document.getElementById('errorMessage');
             if (errorMessage) {
                 document.body.style.overflow = 'hidden';
                 setTimeout(() => {
                     closeMessage('errorMessage');
-                }, 3000); 
+                }, 3000);
             }
         };
     </Script>
