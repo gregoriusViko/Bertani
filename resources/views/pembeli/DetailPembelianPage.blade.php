229f230e523a7f07a3c5297ebf45fe5531cf3ee9
beetahdd <gregoriusangger93@gmail.com>
2024-12-16 23:48:50 +0700

    nambah id objek

--- resources/views/pembeli/DetailPembelianPage.blade.php
+++ resources/views/pembeli/DetailPembelianPage.blade.php
@@ -6,7 +6,7 @@
         <h1 class="text-xl md:text-3xl font-bold tracking-tight text-gray-900">Detail Pembelian</h1>
         <div class="flex gap-x-2">
 
-            <a href="{{ route('laporan.form', ['orderId' => $order->id]) }}" 
+            <a href="{{ route('laporan.form', ['orderId' => $order->id]) }}" id="laporkan"
                 class="inline-flex px-4 py-2 bg-white rounded-lg border border-black gap-x-2 shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-red-500 sm:w-auto">
                 <img src="/img/laporanlaporkan.png" alt="icon_laporkan" class="w-5 h-5">Laporkan
             </a>
@@ -23,16 +23,17 @@
     <div class="border border-gray-300 max-w-4xl mx-auto mt-8 bg-white p-6 rounded-lg">
         <div class="flex flex-col grid-cols-2 md:flex-row gap-x-10">
             <div class="flex-shrink-0 md:pr-16">
-                <p class="text-xl md:text-2xl font-libre-franklin font-bold tracking-tight text-gray-900 py-2">
+                <p id="nameproduk"
+                    class="text-xl md:text-2xl font-libre-franklin font-bold tracking-tight text-gray-900 py-2">
                     {{ $order->product->type->name }}</p>
-                <img src="{{ $order->product->img_link }}" alt="{{ $order->product->type->name }}"
+                <img src="{{ $order->product->img_link }}" alt="{{ $order->product->type->name }}" id="gambarproduk"
                     class="w-40 h-48 object-cover rounded-md shadow-md py-12" />
 
                 <p class="text-base md:text-lg font-libre-franklin font-bold tracking-tight text-gray-900 py-2">Petani
                 </p>
-                <p class="text-base md:text-lg font-libre-franklin italic tracking-tight text-gray-900">
+                <p id="namapetani" class="text-base md:text-lg font-libre-franklin italic tracking-tight text-gray-900">
                     {{ $order->product->farmer->name }}</p>
-                <p class="text-base md:text-lg font-libre-franklin italic tracking-tight text-gray-900">
+                <p id="notelp" class="text-base md:text-lg font-libre-franklin italic tracking-tight text-gray-900">
                     {{ $order->product->farmer->phone_number }}</p>
             </div>
 
@@ -41,37 +42,39 @@
                     class="grid lg:grid-cols-2 gap-x-10 gap-y-8 py-2 text-lg md:text-lg font-libre-franklin tracking-tight text-gray-900 max-w-md">
                     <div>
                         <p class="font-bold">Nomor Pembelian</p>
-                        <p class="italic">{{ $order->receipt_number }}</p>
+                        <p id="idpembelian" class="italic">{{ $order->receipt_number }}</p>
                     </div>
                     <div>
                         <p class="font-bold">Tanggal Pembelian</p>
-                        <p class="italic">{{ $order->order_time->format('d F Y') }}</p>
+                        <p id="tglBeli" class="italic">{{ $order->order_time->format('d F Y') }}</p>
                     </div>
                     <div>
                         <p class="font-bold">Jenis Produk</p>
-                        <p class="italic">{{ $order->product->type->category }}</p>
+                        <p id="jenis" class="italic">{{ $order->product->type->category }}</p>
                     </div>
                     <div>
                         <p class="font-bold">Status</p>
-                        <p class="italic">{{ ucfirst($order->order_status) }}</p>
+                        <p id="statusnya" class="italic">{{ ucfirst($order->order_status) }}</p>
                     </div>
                     <div>
                         <p class="font-bold">Jumlah Beli</p>
-                        <p class="italic">{{ $order->quantity_kg }}</p>
+                        <p id="jumbel" class="italic">{{ $order->quantity_kg }}</p>
                     </div>
                     <div>
                         <p class="font-bold">Metode Pembayaran</p>
-                        <p class="italic">{{ $order->payment_proof }}</p>
+                        <p id="metodenya" class="italic">{{ $order->payment_proof }}</p>
                     </div>
                     <div>
                         <p class="font-bold">Harga</p>
-                        <p class="italic">Rp
+                        <p id="harga" class="italic">Rp
                             {{ number_format($order->historyPrice->price * $order->quantity_kg, 0, ',', '.') }}</p>
                     </div>
                     <div>
                         <p class="font-bold whitespace-nowrap">Alamat Pengambilan Barang</p>
-                        <a href="{{ $order->product->farmer->home_address }}" target="_blank"><p class="italic whitespace-normal">{{ $order->product->farmer->home_address }}
-                        </p></a>
+                        <a id="alamatPetani" href="{{ $order->product->farmer->home_address }}" target="_blank">
+                            <p class="italic whitespace-normal">{{ $order->product->farmer->home_address }}
+                            </p>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -82,36 +85,36 @@
         class="px-8 py-4 font-libre-franklin font-semibold flex flex-col items-center space-y-4 sm:space-y-0 sm:flex-row sm:justify-between sm:items-center">
         <a href="{{ route('chat', $order->product->farmer->slug) }}">
             <button onclick="button" type="button" id="chat"
-            class="inline-flex px-4 py-2 bg-white rounded-lg border border-black gap-x-2 shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-green-500 sm:w-auto">
-            <img src="/img/chat.png" alt="icon_chat" class="w-5 h-5">Chat
-        </button>
+                class="inline-flex px-4 py-2 bg-white rounded-lg border border-black gap-x-2 shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-green-500 sm:w-auto">
+                <img src="/img/chat.png" alt="icon_chat" class="w-5 h-5">Chat
+            </button>
         </a>
         @if ($order->order_status == 'dibatalkan')
-            <button onclick="showAlasanA('showDibatalkan')" id="lihatalasan" type="button"
+            <button onclick="showAlasanA('showDibatalkan')" id="lihatalasanA" type="button"
                 class="text-white inline-flex px-4 py-2 bg-[#f44747] rounded-lg border border-black gap-x-2 shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-red-600 sm:w-auto ">Lihat
                 Alasan
             </button>
         @elseif($order->order_status == 'ditolak')
-            <button onclick="showAlasanB('showDitolak')" id="lihatalasan" type="button"
+            <button onclick="showAlasanB('showDitolak')" id="lihatalasanB" type="button"
                 class="text-white inline-flex px-4 py-2 bg-[#f44747] rounded-lg border border-black gap-x-2 shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-red-600 sm:w-auto ">Lihat
                 Alasan
             </button>
         @endif
 
 
-            @if ($order->order_status == 'menunggu konfirmasi')
-                <button onclick="showPopup('batalkanPesanan')" type="button" id="batalkanPesanan"
-                    class="inline-flex px-4 py-2 bg-white rounded-lg border border-black shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-red-500 sm:w-auto mt-2 sm:mt-0">
-                    Batalkan Pesanan
-                </button>
-            @endif
+        @if ($order->order_status == 'menunggu konfirmasi')
+            <button onclick="showPopup('batalkanPesanan')" type="button" id="batalkanPesanan"
+                class="inline-flex px-4 py-2 bg-white rounded-lg border border-black shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-red-500 sm:w-auto mt-2 sm:mt-0">
+                Batalkan Pesanan
+            </button>
+        @endif
 
-            @if ($order->order_status == 'pesanan diterima')
-                <a href="{{ route('order.showPaymentPage', ['orderId' => $order->id]) }}"
-                    class="inline-flex px-4 py-2 bg-white rounded-lg border border-black shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-blue-500 sm:w-auto mt-2 sm:mt-0">
-                    Pembayaran
-                </a>
-            @endif
+        @if ($order->order_status == 'pesanan diterima')
+            <a href="{{ route('order.showPaymentPage', ['orderId' => $order->id]) }}"
+                class="inline-flex px-4 py-2 bg-white rounded-lg border border-black shadow hover:shadow-md hover:border-opacity-10 transition-shadow hover:bg-blue-500 sm:w-auto mt-2 sm:mt-0">
+                Pembayaran
+            </a>
+        @endif
     </div>
 
 
@@ -127,10 +130,10 @@
                     placeholder="Masukkan alasan pembatalan"></textarea>
                 <div class="flex justify-end gap-4">
                     <button type="button" onclick="closePopup()"
-                        class="border border-black inline-flex px-4 py-2 bg-white rounded-lg gap-x-2 hover:bg-gray-200">
+                       id="tutup" class="border border-black inline-flex px-4 py-2 bg-white rounded-lg gap-x-2 hover:bg-gray-200">
                         <img src="/img/laporanbatal.png" alt="batalkanPesanan" class="w-5 h-5">BATAL</button>
                     <button type="submit"
-                        class="border border-black inline-flex px-4 py-2 bg-white rounded-lg gap-x-2 hover:bg-gray-200">
+                        id="kirim" class="border border-black inline-flex px-4 py-2 bg-white rounded-lg gap-x-2 hover:bg-gray-200">
                         <img src="/img/paperplane.png" alt="batalkanPesanan" class="w-5 h-5">KIRIM</button>
                 </div>
             </form>
@@ -141,10 +144,11 @@
         <div class="grid grid-flow-row">
             <div class="text-xl font-bold mb-4">Alasan Pembatalan</div>
             <div class="border border-black rounded-md p-2 h-24 scroll-auto">
-                <h3>{{ $order->cancellation_reason }}</h3>
+                <h3 id="alasandibatal">{{ $order->cancellation_reason }}</h3>
             </div>
             <div class="mt-4">
-                <button type="button" class="bg-gray-400 px-2 py-1 rounded-md hover:bg-gray-700 hover:text-white" id="closeA" onclick="closeModal('showDibatalkan')">TUTUP</button>
+                <button type="button" class="bg-gray-400 px-2 py-1 rounded-md hover:bg-gray-700 hover:text-white"
+                    id="closeA" onclick="closeModal('showDibatalkan')">TUTUP</button>
             </div>
         </div>
     </x-Modal>
@@ -153,10 +157,11 @@
         <div class="grid grid-flow-row">
             <div class="text-xl font-bold mb-4">Alasan Pembatalan</div>
             <div class="border border-black rounded-md p-2 h-24 scroll-auto">
-                <h3>{{ $order->cancellation_reason }}</h3>
+                <h3 id="alasanditolak">{{ $order->cancellation_reason }}</h3>
             </div>
             <div class="mt-4">
-                <button type="button" class="bg-gray-400 px-2 py-1 rounded-md hover:bg-gray-700 hover:text-white" id="closeB" onclick="closeModal('showDitolak')">TUTUP</button>
+                <button type="button" class="bg-gray-400 px-2 py-1 rounded-md hover:bg-gray-700 hover:text-white"
+                    id="closeB" onclick="closeModal('showDitolak')">TUTUP</button>
             </div>
         </div>
     </x-Modal>
